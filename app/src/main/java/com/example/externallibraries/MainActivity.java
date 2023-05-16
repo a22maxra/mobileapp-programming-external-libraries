5da69650c0dc59d68c9c49c5bd42fc6e3e81ee93
Max Rapp <max.rapp01@live.se>
2023-05-16 22:40:38 +0200

    Display picture

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.externallibraries;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.imageView);
+        String imageUrl = "https://cdn.pixabay.com/photo/2023/05/11/05/40/blackbird-7985552_960_720.jpg";
+
+        Glide.with(this)
+                .load(imageUrl)
+                .apply(new RequestOptions().centerCrop())
+                .into(imageView);
     }
 }
